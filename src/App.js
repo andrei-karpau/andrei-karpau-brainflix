e0751ddbef0965776650a5c09b2f4f87ee8f29b7
Andrei Karpau <andrei.karpau@icloud.com>
2024-03-10 00:01:29 -0800

    initial header bone and project sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// I had to add "--openssl-legacy-provider" option to start and build script otherwise npm start failed:(
+  import React  from 'react';
+import './App.scss';
+import Header from './components/Header/Header'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,31 @@
+import React from 'react';
 import './Header.scss';
+import logo from '../../assets/Logo/BrainFlix-logo.svg'
+import avatar from '../../assets/Images/Mohan-muruge.jpg'
+
 
 function Header () {
-    <>
-    </>
+
+    return (
+        <header>
+            <nav className='nav-bar'>
+                <div className='nav-bar__container'>
+                    <div className='nav-bar__container-logo'>
+                        <a className='nav-bar__container-logo--link' href='#brainflix'>
+                            <img id='logo' src={logo} alt='Here could be Brain Flix Logo'/>
+                        </a>
+                    </div>
+                    <div className='nav-bar__container-search'>
+                        <input type='search' placeholder='Search' id='search-input' className='nav-bar__container-search--input' />
+                    </div>
+                    <img className='nav-bar__container-search--avatar' src={avatar}  alt='here could be user avatar'/>
+                    <button className='nav-bar__container-upload'>
+                        upload
+                    </button>
+                </div>
+            </nav>
+        </header>
+    );
 }
+
+export default Header;
