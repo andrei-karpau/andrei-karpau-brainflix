76c1d78b53e11d75f1c947c1b50294d1017e83e2
Andrei Karpau <andrei.karpau@icloud.com>
2024-04-13 23:10:07 -0700

    after repo rename run git remote set-url origin NEW_URL

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import VideoUploadPage from './pages/VideoUploadPage/VideoUploadPage';
 
-//test git push do not return warning on remote origin
-
 function App() {
 
   return (
